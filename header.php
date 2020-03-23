e568640fe8ff4fb680926f9c2cf609231edc725c
Raul-Araujo <raul31_lpcn@hotmail.com>
2020-03-23 00:03:37 -0300

    incusão head

--- header.php
+++ header.php
@@ -1,15 +1,30 @@
-<!DOCTYPE html>
-<html lang="pt-br">
+   <!--Inicio do NAV MENU-->
+   <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <a class="navbar-brand" href="../ProjetoDH/index.php">Home</a>
+           <h3>| </h3>
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+              <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                <li class="nav-item active">
+                  <a class="nav-link" href="../ProjetoDH/produtos.php"> Produtos</a>
+                </li>
+                <li class="nav-item active">
+                  <a class="nav-link" href="../ProjetoDH/quem-somos.php">Quem somos</a>
+                </li>
+                <li class="nav-item active">
+                  <a class="nav-link" href="../ProjetoDH/contato.php">Contato</a>
+                </li>
+              </ul>
+                 <!--Botão de login-->
+              <form class="form-inline my-2 my-lg-0">
+                <button class="btn my-2 my-sm-0" type="button"><img src="../ProjetoDH/css/img/header/login.png" height="35" width="35"alt="botão de LogIn" data-toggle="modal" data-target="#exampleModal"></button>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link  rel="stylesheet" href ="../ProjetoDH/css/style.css">
-    <title>SurfWin</title>
-
-</head>
-
-<body >
-
+                <!--Botões do carrinho de compra-->
+                <button class="btn my-2 my-sm-0" type="submit"><img src="../ProjetoDH/css/img/header/carrinho.png" height="35" width="35"alt="botão do carrinho"></button>
+              </form>
+            </div>
+          </nav>   
+    </header>
