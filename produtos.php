4222fd8891e61473d35a4428208dac6108c1b5f2
Teresa Penteado <teresapenteado@hotmail.com>
2020-03-27 20:13:16 -0300

    incluisao da tela de produtos

--- produtos.php
+++ produtos.php
@@ -11,7 +11,131 @@
  <!--Inicio do MAIN-->
  <main>
     <h2 class="titulosETextos">Produtos</h2>
+    <div class="container my-3">
 
+      <div class="row">
+      <div class="col-md-6 col-lg-3 mb-sm-3">
+         <div class="card h-80">
+            <img src="css/img/surf001.jpg" class="card-img-top" alt="prancha 1">
+            <div class="card-body">
+            <h5 class="card-title">Surfboard Neptune</h5>
+            <p class="card-text">De 40cm x 20cm - R$150,00</p>
+            <div class="d-flex flex-row-reverse">
+                  <a class="icons" href=""><img class="icons" src="css/img/header/shopping-bag.png" 
+                  width="23px" alt="bolsa"></a>
+                  <a class="icons" href=""><img class="icons" src="css/img/header/heart.png"
+                  width="23px" alt="coracao"></a>
+            </div>
+            </div>
+         </div>
+      </div>
+      <div class="col-md-6 col-lg-3 mb-sm-3">
+         <div class="card h-80">
+            <img src="css/img/surf002.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5 class="card-title">Surfboard Hercules</h5>
+            <p class="card-text">De xx cm x xx cm - R$ xxx,xx</p>
+            <div class="d-flex flex-row-reverse">
+               <a class="icons" href=""><img class="icons" src="css/img/header/shopping-bag.png"
+               width="23px" alt="bolsa"></a>
+               <a class="icons" href=""><img class="icons" src="css/img/header/heart.png"
+               width="23px" alt="coracao"></a>
+            </div>
+            </div>
+         </div>
+      </div>
+      <div class="col-md-6 col-lg-3 mb-sm-3">
+         <div class="card h-80">
+            <img src="css/img/surf003.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5 class="card-title">Surfboard Poseidon</h5>
+            <p class="card-text">De xx cm x xx cm - R$ xxx,xx</p>
+            <div class="d-flex flex-row-reverse">
+               <a class="icons" href=""><img class="icons" src="css/img/header/shopping-bag.png"
+               width="23px" alt="bolsa"></a>
+               <a class="icons" href=""><img class="icons" src="css/img/header/heart.png"
+               width="23px" alt="coracao"></a>
+            </div>
+            </div>
+         </div>
+      </div>
+      <div class="col-md-6 col-lg-3 mb-sm-3">
+         <div class="card h-80">
+            <img src="css/img/surf004.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5 class="card-title">Surfboard Apollo</h5>
+            <p class="card-text">De xx cm x xx cm - R$ xxx,xx</p>
+            <div class="d-flex flex-row-reverse">
+               <a class="icons" href=""><img class="icons" src="css/img/header/shopping-bag.png"
+               width="23px" alt="bolsa"></a>
+               <a class="icons" href=""><img class="icons" src="css/img/header/heart.png"
+               width="23px" alt="coracao"></a>
+            </div>
+            </div>
+         </div>
+      </div>
+      <div class="col-md-6 col-lg-3 mb-sm-3">
+         <div class="card h-80">
+            <img src="css/img/surf005.jpeg" class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5 class="card-title">Surfboard Artemis</h5>
+            <p class="card-text">de 40cm x 20cm - R$ 150,00</p>
+            <div class="d-flex flex-row-reverse">
+               <a class="icons" href=""><img class="icons" src="css/img/header/shopping-bag.png"
+               width="23px" alt="bolsa"></a>
+               <a class="icons" href=""><img class="icons" src="css/img/header/heart.png"
+               width="23px" alt="coracao"></a>
+            </div>
+            </div>
+         </div>
+      </div>
+      <div class="col-md-6 col-lg-3 mb-sm-3">
+         <div class="card h-80">
+            <img src="css/img/surf006.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5 class="card-title">Surfboard Demeter</h5>
+            <p class="card-text">De xx cm x xx cm - R$ xxx,xx</p>
+            <div class="d-flex flex-row-reverse"> 
+               <a class="icons" href=""><img class="icons" src="css/img/header/shopping-bag.png"
+                  width="23px" alt="bolsa"></a>
+               <a class="icons" href=""><img class="icons" src="css/img/header/heart.png"
+                  width="23px" alt="coracao"></a>
+            </div>
+            </div>
+         </div>
+      </div>
+      <div class="col-md-6 col-lg-3 mb-sm-3">
+         <div class="card h-80">
+            <img src="css/img/surf007.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5 class="card-title">Surfboard Zeus</h5>
+            <p class="card-text">De 40cm x 20cm - R$ 150,00</p>
+            <div class="d-flex flex-row-reverse">
+               <a class="icons" href=""><img class="icons" src="css/img/header/shopping-bag.png"
+                  width="23px" alt="bolsa"></a>
+               <a class="icons" href=""><img class="icons" src="css/img/header/heart.png"
+               width="23px" alt="coracao"></a>
+            </div>
+            </div>
+         </div>
+      </div>
+      <div class="col-md-6 col-lg-3 mb-sm-3">
+         <div class="card h-80">
+            <img src="css/img/surf008.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5 class="card-title">Surfboard Zeus</h5>
+            <p class="card-text">De 40cm x 20cm - R$ 150,00 </p>
+            <div class="d-flex flex-row-reverse">
+               <a class="icons" href=""><img class="icons" src="css/img/header/shopping-bag.png"
+               width="23px" alt="bolsa"></a>
+               <a class="icons" href=""><img class="icons" src="css/img/header/heart.png" 
+               width="23px" alt="coracao"></a>
+            </div>
+            </div>
+         </div>
+      </div>
+      </div>
+      </div>
  </main>
 
 
