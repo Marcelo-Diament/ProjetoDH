3bd40a7d94a32733902338004ad0e266e617f618
Marcelo Diament <marcelodiament@gmail.com>
2020-03-27 23:21:13 -0300

    Atualizando utils/usuario
    Atualização da função que capta o arquivo e transforma a string do JSON em array (e depois compara com o id da session para definir o $usuarioEncontrado

--- utils/usuario.php
+++ utils/usuario.php
@@ -1,8 +1,19 @@
 <?php
 // CAPTURANDO DADOS DO USUÁRIO LOGADO NO JSON
-//$usuarioEncontrado = getRegisters("usuarios", ["email", $_REQUEST["email"]]);
-$usuarioEncontrado = file_get_contents('./data/usuarios.json');
-
+// AQUI TEMOS O CONTEÚDO DO ARQUIVO JSON (UMA STRING)
+$arquivo = file_get_contents('./data/usuarios.json'); // <= O erro cemeçou aqui, não havia o json_decode nem o loop para achar o user
+// AQUI TRANSFORMAMOS A STRING EM ARRAY
+$arrayDosUsuarios = json_decode($arquivo, true);
+// AQUI ACHAMOS O USUÁRIO ENCONTRADO ATRAVÉS DO EMAIL DELE (MAS PRECISA SER ENVIADO UM $_REQUEST COM O ÍNDICE email E UM VALOR)
+// $usuarioEncontrado = getRegisters("usuarios", ["email", $_REQUEST["email"]]);
+// OBSERVAÇÃO: VI QUE PEGARAM DAQUELE MEU REPOSITÓRIO COMO BASE, MAS CUIDADO QUE LÁ TÁ UM POUCO MAIS COMPLEXO, POIS AS FUNÇÕES FORAM FEITAS PARA RECEBER UM JSON MAIS COMPLEXO TB. DE FORMA GERAL, ESSA FUNÇÃO getRegisters() DEVE RECEBER O ÍNDICE DENTRO DO ARRAY (usuarios) E UM ARRAY QUE RECEBE O PARÂMETRO A SER ANALISADO E O VALOR DESSE PARÂMETRO. PORÉM NÃO EXISTE ESSA FUNÇÃO NO PROJETO DE VOCÊS. ENTÃO FIZ UM LOOP AQUI PRA GENTE ACHAR ESSE USUÁRIO PELO ID TRAZIDO PELA SESSION (DEFINIDA NO LOGIN.PHP)
+if(isset($_SESSION) && isset($_SESSION["id"])):
+    foreach($arrayDosUsuarios["usuarios"] as $usuario):
+        if($usuario["id"] === $_SESSION["id"]):
+            $usuarioEncontrado = $usuario;
+        endif;
+    endforeach;
+endif;
 // /CAPTURANDO DADOS DO USUÁRIO LOGADO NO JSON
 
 // EDITANDO REGISTRO NO JSON
